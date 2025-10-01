55c67139933f4f20309d12d2ce58af75901ddf99
vikas-kashyap97 <your_github_email@example.com>
2025-10-01 12:27:28 +0530

    openai tracing

---
+++ Tracing/01_openai_tracing.py
@@ -0,0 +1,26 @@
+from agents import Agent, Runner, OpenAIChatCompletionsModel, trace, set_default_openai_api, set_default_openai_client, set_tracing_disabled
+import asyncio
+import os
+from openai import AsyncOpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
+client = AsyncOpenAI()
+set_default_openai_client(client=client, use_for_tracing=True)
+set_default_openai_api("chat_completions")
+
+async def main():
+    agent = Agent(name="Joke generator", instructions="generate a context about the query asked by the user, then tell a joke based on that context, and finally rate the joke on a scale of 1 to 10",)
+    user_input = input("Ask me anything (e.g., 'Tell me a joke'): ")
+
+    with trace("Joke workflow"): 
+        first_result = await Runner.run(agent, user_input)
+        second_result = await Runner.run(agent, f"Rate this joke: {first_result.final_output}")
+        print(f"Joke: {first_result.final_output}")
+        print(f"Rating: {second_result.final_output}")
+
+asyncio.run(main())
